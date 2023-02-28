6e67cdec7cad27736f11ead1b4a39a7c14e5f7a5
hugo.coleau <45538763+hugo59400@users.noreply.github.com>
2023-02-28 16:03:10 +0100

    feature crud

--- src/produit/produit.module.ts
+++ src/produit/produit.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProduitService } from './produit.service';
 import { ProduitController } from './produit.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Produit } from './entities/produit.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Produit])],
   controllers: [ProduitController],
   providers: [ProduitService]
 })
--- src/produit/produit.service.ts
+++ src/produit/produit.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException  } from '@nestjs/common';
 import { CreateProduitDto } from './dto/create-produit.dto';
 import { UpdateProduitDto } from './dto/update-produit.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { Produit } from './entities/produit.entity';
 
 @Injectable()
 export class ProduitService {
+
+
+  constructor(
+    @InjectRepository(Produit)
+    private produits: Repository<Produit>,
+  ) {}
+
+
+
   create(createProduitDto: CreateProduitDto) {
-    return 'This action adds a new produit';
+    const produit = this.produits.create(createProduitDto);
+    return this.produits.save(produit);
   }
 
-  findAll() {
-    return `This action returns all produit`;
+  async findAll() {
+    return await this.produits.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} produit`;
+  async findOne(id: number) {
+    const result = await this.produits.findOneBy({ id });
+    if (result === null) {
+      throw new NotFoundException();
+    }
+    return result;
   }
 
-  update(id: number, updateProduitDto: UpdateProduitDto) {
-    return `This action updates a #${id} produit`;
+  async update(id: number, updateProduitDto: UpdateProduitDto) {
+    const update = await this.produits.update({ id }, updateProduitDto);
+    if (update.affected === 0) {
+      throw new NotFoundException();
+    }
+    return update;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} produit`;
+  async remove(id: number) {
+    const deleteResponse = await this.produits.delete(id);
+    if (deleteResponse.affected === 0) {
+      throw new NotFoundException();
+    }
+    return deleteResponse;
   }
 }
